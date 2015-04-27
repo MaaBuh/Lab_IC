4cd1bfa2ba1c381c28c3358b8ee174265f083012
mernerp <mernerp@gmail.com>
2015-04-27 07:23:18 +0200

    Erste DOM Funktionalität hinzugefügt. ToDo: Einzelne Nodes anhand von ID und tag einzeln auslesen können. ToDo: Nodes zerlegen und in die Postgre Datenbank schieben können.

--- LabIC/src/Main.java
+++ LabIC/src/Main.java
@@ -1,15 +1,49 @@
+import java.io.IOException;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		Connection conn = new Connection();
-		System.out.println("########");
+//		Node node = new Node();
 		
 		PostgreFunctions var1 = new PostgreFunctions();
 		var1.createTable(conn.getPostgreConnection());
 		var1.dropTable(conn.getPostgreConnection());
+		
+        Document doc = null;
+        try {
+//        	Original file
+//            doc = OSMReader.readXMLDocument("res/map(1)");
+        	
+//        	Gekürzte file
+        	doc = OSMReader.readXMLDocument("res/map(1)_geschnitten");
+        	
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(OSMReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(OSMReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(OSMReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        OSMReader.PrintToConsole(doc);
+//        OSMReader.PrintToConsole(doc.getElementById("21402303"));
+
+        
+        
+        
+
+        
+//        System.out.println(doc.toString());
 		
 		
 //		Statement stmt = conn.createStatement(); 
--- LabIC/src/OSMReader.java
+++ LabIC/src/OSMReader.java
@@ -1,35 +1,31 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
+
+import java.io.StringWriter;
 
 import javax.xml.parsers.DocumentBuilder; 
 import javax.xml.parsers.DocumentBuilderFactory; 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document; 
 import org.w3c.dom.Node; 
 import org.w3c.dom.NodeList; 
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
 import org.xml.sax.SAXException;
 
 
 public class OSMReader {
 	
-    public static void main(String[] args)  {
-        Document doc = null;
-        try {
-            doc = readXMLDocument("res/map(1)");
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(OSMReader.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SAXException ex) {
-            Logger.getLogger(OSMReader.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(OSMReader.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println( );
-    }
-
-
 	public static Document readXMLDocument(String filename)  
 			throws ParserConfigurationException, SAXException, IOException{ 
 		DocumentBuilderFactory factory  = DocumentBuilderFactory.newInstance(); 
@@ -39,5 +35,27 @@ public class OSMReader {
 		document = builder.parse( new File( filename ) ); 
 		return document; 
 	} 
+	
+    /*
+     * Ausgelesenes Dokument in Konsolge anzeigen
+     */
+	public static void PrintToConsole(Document doc) {
+        DOMImplementationLS domImplLS = (DOMImplementationLS) doc
+            .getImplementation();
+        LSSerializer serializer = domImplLS.createLSSerializer();
+        String str = serializer.writeToString(doc);
+        System.out.println(str);
+	}
+    /*
+     * Ausgelesenen Node in Konsolge anzeigen
+     * Funktioniert noch nicht richtig
+     */
+//	public static void PrintToConsole(Node node) {
+//		StringWriter writer = new StringWriter();
+//		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+//		transformer.transform(new DOMSource(node), new StreamResult(writer));
+//		String str = writer.toString();
+//	}
+//	
 }
 
